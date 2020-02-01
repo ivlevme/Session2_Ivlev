21fdbf393ab91ee0e9f82831c6c3ae4eda478c54
ivlev <ivlev.me@gmail.com>
2020-02-01 11:38:26 +0300

    форма авторизации

--- Session1_Ivlev/Form1.Designer.cs
+++ Session1_Ivlev/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.warningLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(241, 221);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(247, 22);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(241, 270);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(247, 22);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(241, 353);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 33);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(413, 353);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 33);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(181, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 128);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.AutoSize = true;
+            this.warningLabel.Location = new System.Drawing.Point(311, 179);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(0, 17);
+            this.warningLabel.TabIndex = 7;
+            this.warningLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(692, 413);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label warningLabel;
     }
 }
 
--- Session1_Ivlev/Form1.cs
+++ Session1_Ivlev/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,120 @@ namespace Session1_Ivlev
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Timer tmrShow = new Timer();
+
+        void unlockForm(object sender, EventArgs e)
+        {
+            tmrShow.Stop();
+            tmrShow.Tick -= new System.EventHandler(unlockForm);
+            usernameTextBox.ReadOnly = false;
+            passwordTextBox.ReadOnly = false;
+            loginButton.Enabled = true;
+
+            warningLabel.Text = tmrShow.ToString();
+        }
+
+
+        int countAttempt = 3;
+
+        private async void loginButton_Click(object sender, EventArgs e)
+        {
+            warningLabel.Visible = false;
+
+            SqlConnection sqlConnection;
+
+            string connectionPath = @"Data Source = localhost; 
+                Initial Catalog = Session1_Ivlev; 
+                Integrated Security = True; 
+                Connect Timeout = 30; 
+                Encrypt = False; 
+                TrustServerCertificate = False; 
+                ApplicationIntent = ReadWrite; 
+                MultiSubnetFailover = False";
+
+            sqlConnection = new SqlConnection(connectionPath);
+            await sqlConnection.OpenAsync();
+
+            SqlDataReader sdr = null;
+
+            SqlCommand cmdSelect = new SqlCommand("SELECT * FROM [Users]", sqlConnection);
+
+            bool checkLogin = false;
+
+
+            try
+            {
+                sdr = await cmdSelect.ExecuteReaderAsync();
+                while (await sdr.ReadAsync())
+                {
+                    if (usernameTextBox.Text == Convert.ToString(sdr["Email"]))
+                    {
+                        checkLogin = true;
+
+                        if (passwordTextBox.Text == Convert.ToString(sdr["Password"]))
+                        {
+                            MessageBox.Show("Успешно");
+
+                            break;
+
+                        } else
+                        {
+                            warningLabel.Text = "Пароль неверный!";
+                            warningLabel.Visible = true;
+
+                            countAttempt--;
+                            break;
+                        }
+                    } else
+                    {
+                        warningLabel.Text = "Логин неверный!";
+                        warningLabel.Visible = true;
+
+                        countAttempt--;
+                        break;
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (countAttempt == 0)
+            {
+                usernameTextBox.ReadOnly = true;
+                passwordTextBox.ReadOnly = true;
+                loginButton.Enabled = false;
+
+
+                tmrShow.Interval = 10000;
+                tmrShow.Tick += new System.EventHandler(unlockForm);
+                tmrShow.Start();
+
+                warningLabel.Visible = true;
+                warningLabel.Text = "Авторизация заблокирована на 10 секунд!";
+ 
+                countAttempt = 3;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
